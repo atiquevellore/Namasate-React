452b77546cf0a039acd280b24823af289589729a
atique bokhari <atiquevellore@gmail.com>
2023-08-20 13:09:22 +0530

    created nested html structure using React.creatElement

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// creating a nested html strutucture using React.createElement()
+/* <div>
+    <div>
+        <h1>i am h1 tag</h1>
+        <h2> i am h2 tag</h2>
+    </div>
+    <div>
+        <h1>i am h3 tag</h1>
+        <h2> i am h4 tag</h2>
+    </div>
+</div> */
+
+const parent = React.createElement("div",{id: "parent"},
+[React.createElement("div",{id: "child"},
+[React.createElement("h1",{},"i am h1 tag"),React.createElement("h1",{},"i am h2 tag")]),
+[React.createElement("div",{id: "child2"},
+[React.createElement("h1",{},"i am h3 tag"),React.createElement("h1",{},"i am h4 tag")])
+]])
+console.log(parent)
 const heading_3 = React.createElement("h3",{id: 'heading'},"Hello World from React! ");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading_3);
+root.render(parent);
