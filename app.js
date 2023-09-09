34facb41aa06fc989debeead422b6ad88fb4d7aa
atique bokhari <atiquevellore@gmail.com>
2023-09-09 19:49:02 +0530

    removed cdn and using react through parcel

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-// creating a nested html strutucture using React.createElement()
-/* <div>
-    <div>
-        <h1>i am h1 tag</h1>
-        <h2> i am h2 tag</h2>
-    </div>
-    <div>
-        <h1>i am h3 tag</h1>
-        <h2> i am h4 tag</h2>
-    </div>
-</div> */
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div",{id: "parent"},
-[React.createElement("div",{id: "child"},
-[React.createElement("h1",{},"i am h1 tag"),React.createElement("h1",{},"i am h2 tag")]),
-[React.createElement("div",{id: "child2"},
-[React.createElement("h1",{},"i am h3 tag"),React.createElement("h1",{},"i am h4 tag")])
-]])
+const parent = React.createElement("div",{"id":"parent"},[React.createElement("div",{"id":"childs"},
+    [React.createElement("h2",{},"i am h2 tag")],React.createElement("div",{},
+        [React.createElement("h3",{},"i am h3 tag"),React.createElement("h4",{},"react hmr check")]))])
 console.log(parent)
 const heading_3 = React.createElement("h3",{id: 'heading'},"Hello World from React! ");
 const root = ReactDOM.createRoot(document.getElementById("root"));
