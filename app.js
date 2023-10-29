52353a006f6c70d4fe1d137053543ffef786a7f9
atique bokhari <atiquevellore@gmail.com>
2023-10-29 11:59:01 +0530

    using jsx instead of react createlement(Code Readability)

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div",{"id":"parent"},[React.createElement("div",{"id":"childs"},
-    [React.createElement("h2",{},"i am h2 tag")],React.createElement("div",{},
-        [React.createElement("h3",{},"i am h3 tag"),React.createElement("h4",{},"react hmr check")]))])
-console.log(parent)
-const heading_3 = React.createElement("h3",{id: 'heading'},"Hello World from React! ");
+const heading = React.createElement("h1",{"id":"textcolor"},"React using the React Element");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+//JSX - to write more readable code
+const jsxheading = <h1 className="text-color">React using JSX</h1>
+
+root.render(jsxheading)
