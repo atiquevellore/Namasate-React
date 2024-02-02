48e3d80b6826b8c42ea3b9889afba2f2088d5531
atique <atiquevellore@gmail.com>
2024-02-03 00:18:11 +0530

    fetched delivery time from the api

--- src/Components/ResturantCard.js
+++ src/Components/ResturantCard.js
@@ -1,7 +1,8 @@
-import { IMAGE_CDN_URL } from "../Utils/constants";
+import { IMAGE_CDN_URL } from "../Utils/Constants";
 const ResturantCard = ({ resdata }) => {
 	const { name, cuisines, avgRatingString, costForTwo, cloudinaryImageId } =
 		resdata;
+	const { slaString } = resdata?.sla;
 	return (
 		<div className="res-card">
 			<img className="res-logo" src={IMAGE_CDN_URL + cloudinaryImageId} />
@@ -9,6 +10,7 @@ const ResturantCard = ({ resdata }) => {
 			<h4>{cuisines.join(", ")}</h4>
 			<h4>{avgRatingString}</h4>
 			<h4>{costForTwo}</h4>
+			<h4>{slaString}</h4>
 		</div>
 	);
 };
